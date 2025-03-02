13f1eab0edc6892d1c60f30a8fbaff13460c98dc
giftontoolkit <giftontoolkit@gmail.com>
2025-03-02 06:44:31 +0500

    Add files via upload

--- v1/functions.php
+++ v1/functions.php
@@ -1,11 +1,11 @@
 <?php
 // Theme Setup - register-navigation-menu
-function gifton_theme_setup_rn_menu() {
+function myTM_theme_setup_rn_menu() {
 
      // Register navigation menu
      register_nav_menus(array(
-        'primary' => __('Primary Menu', 'gifton-theme')
+        'primary' => __('Primary Menu', 'myTM-theme')
     ));
 }
-add_action('after_setup_theme', 'gifton_theme_setup_rn_menu');
+add_action('after_setup_theme', 'myTM_theme_setup_rn_menu');
 ?>
